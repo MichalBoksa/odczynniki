8a2b891d3b05b541f985bdc1fd8aced8774f3400
MichalBoksa <michalboksa1404@gmail.com>
2024-04-24 21:55:40 +0200

    Revert "Merge branch 'main' of github.com:sanity-io/nextjs-blog-cms-sanity-v3"
    This reverts commit 86e739bc622d153c41ec6e7f5796ff3a58b0eed1, reversing
    changes made to ec75845d159711dd07ba9c0101b6482d3e2045c3.

--- next.config.mjs
+++ next.config.mjs
@@ -2,22 +2,3 @@
 const nextConfig = {};
 
 export default nextConfig;
-/** @type {import('next').NextConfig} */
-const config = {
-  images: {
-    remotePatterns: [
-      { hostname: 'cdn.sanity.io' },
-      { hostname: 'source.unsplash.com' },
-    ],
-  },
-  typescript: {
-    // Set this to false if you want production builds to abort if there's type errors
-    ignoreBuildErrors: process.env.VERCEL_ENV === 'production',
-  },
-  eslint: {
-    /// Set this to false if you want production builds to abort if there's lint errors
-    ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
-  },
-}
-
-export default config

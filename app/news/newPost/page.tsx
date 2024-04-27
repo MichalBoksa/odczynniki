49db3e76dc8f59a1ddff7d5c5205c7223490efad
MichalBoksa <michalboksa1404@gmail.com>
2024-04-27 16:16:29 +0200

    added new post editor

---
+++ app/news/newPost/page.tsx
@@ -0,0 +1,10 @@
+import NewPostEditor from '@/components/NewPostEditor'
+import React from 'react'
+
+const page = () => {
+  return (
+    <NewPostEditor />
+  )
+}
+
+export default page
---
+++ components/NewPostEditor.tsx
@@ -0,0 +1,40 @@
+"use client";
+import Image from 'next/image'
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; 
+
+const NewPostEditor = () => {
+
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState('')
+
+  return (
+   <section className='max-container padding-container '>
+    <input type="text" className='input text-6xl mb-4 rounded p-12 b-none outline-none bg-transparent' placeholder='Tytuł'/>
+    
+        <div className='flex flex-col gap-5 h-[700px]'>
+            <button className='flex w-[36px] h-[36px] rounded-[50%] border-2 items-center justify-center' >
+                <Image src="/plus.png" alt="" width={16} height={16} onClick={() => setOpen(!open)} />
+            </button>
+            {open && (
+            <div className='flex flex-row gap-4 '>
+                <button className='flex w-[36px] h-[36px] rounded-[50%] border-2 items-center justify-center '>
+                    <Image src="/image.png" alt="" width={16} height={16} />
+                </button>
+                <button className='flex w-[36px] h-[36px] rounded-[50%] border-2 items-center justify-center'>
+                    <Image src="/external.png" alt="" width={16} height={16} />
+                </button>
+                <button className='flex w-[36px] h-[36px] rounded-[50%] border-2 items-center justify-center'>
+                    <Image src="/video.png" alt="" width={16} height={16} />
+                </button>
+            </div>)}
+            <ReactQuill theme='bubble' placeholder='Treść posta...' value={value} onChange={setValue} />
+            <button className='flex btn-active btn-primary btn-sm rounded-lg items-center ml-auto mr-[25%]'>Dodaj wpis</button>
+        </div> 
+        
+    </section>
+  )
+}
+
+export default NewPostEditor

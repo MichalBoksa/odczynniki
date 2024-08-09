cd84aec8298c7955764441bbe237b2ad58643f41
MichalBoksa <michalboksa1404@gmail.com>
2024-08-09 13:22:26 +0200

    added responvieness to aboutUs and molybdenymInfo

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -40,7 +40,7 @@ const AboutUs = () => {
        <div className='bg-dark-gray flex flex-col md:max-w-[40%] lg:px-8 items-center justify-center' >
 
        <div className='flex flex-col '>
-           <div className="flex mb-8 mt-8 lg:px-16 text-center text-cream " >
+           <div className="flex mb-8 mt-8 px-3 lg:px-16 text-center text-cream " >
                <p className=' text-left font-bold  md:text-md lg:text-2xl'>{data?.SPECIALIZATION}</p>
            </div>
            <div className='flex px-8 mb-8 text-cream'>
@@ -80,21 +80,22 @@ const AboutUs = () => {
 
    <CerificatesSlider/>
 
-   <div className='flex flex-col md:flex-row bg-dark-gray text-cream max-container w-full lg:min-h-[250px] tracking-wid p-3'>
+   <div className='flex flex-col md:flex-row bg-dark-gray text-cream max-container w-full lg:min-h-[250px] tracking-wid p-3 '>
        
-       <div className='flex flex-col w-[60%] items-start justify-center'>     
+       <div className='flex flex-col w-[60%] items-start justify-center order-2 md:order-1 space-y-2'>     
         
         <div className="flex items-center">
             <CldImage src='https://res.cloudinary.com/dozgr1muo/image/upload/v1234/midas/ijt9xa0obuhvt6sbw2o3.svg' width={22} height={22} alt='home icon' className='mr-2'/>
             <a className="">{data?.OFFICE_ADDRESS}</a>
         </div>
         
-        <div className="flex items-center">
+        <div className="flex items-center w-full">
             <CldImage src='https://res.cloudinary.com/dozgr1muo/image/upload/v1234/midas/oee1rkckq36hdhtpmox5.svg' width={21} height={21} alt='phone icon' className='mr-2'/>
-            <a className="">{data?.TEL1} </a>
-            <a className='ml-2'>{data?.TEL2}</a>
-            <a className='ml-2 float-left clear-left'>{data?.FAX}</a>
-            
+                <div className="flex flex-col sm:flex-row">
+                    <a className="">{data?.TEL1} </a>
+                    <a className='ml-6 sm:ml-2'>{data?.TEL2}</a>
+                    <a className='ml-2 float-left clear-left'>{data?.FAX}</a>
+                </div>
         </div>
 
         <div className="flex items-center">
@@ -102,7 +103,7 @@ const AboutUs = () => {
             <a className="">{data?.OFFICE_EMAIL}</a>
         </div>
 
-        <div className="flex items-start">
+        <div className="flex items-start ">
             <CldImage src='https://res.cloudinary.com/dozgr1muo/image/upload/v1234/midas/aboutus/hp4dkvu0kg7rhi0dqdzw.svg' width={22} height={22} alt='tax icon' className='mr-2'/>
             <div className="flex flex-col">
                 <a className="">{data?.VAT}: 522-278-77-53</a>
@@ -114,8 +115,8 @@ const AboutUs = () => {
       
     </div>
 
-        <div className=' flex flex-col w-[40%] lg:px-8 items-center justify-center' >
-            <h6 className="text-4xl">{data?.REGISTRATION_DETAILS}</h6> 
+        <div className=' flex flex-col md:w-[40%] lg:px-8 items-center justify-center mb-2' >
+            <h6 className="text-4xl order-1 md:order-2">{data?.REGISTRATION_DETAILS}</h6> 
        </div>
    </div>
                

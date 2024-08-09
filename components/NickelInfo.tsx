e9dcb9ca1fdaab90b8df163ed90a8dfd00397bac
MichalBoksa <michalboksa1404@gmail.com>
2024-08-09 13:36:15 +0200

    added responsiveness to other products

--- components/NickelInfo.tsx
+++ components/NickelInfo.tsx
@@ -1,64 +1,64 @@
-"use client";
+'use client';
 import { useLocale } from '../lang/LocaleContext';
 
 const NickelInfo = () => {
-const { data } = useLocale() || {};
+  const { data } = useLocale() || {};
 
-return (
-    <section className='max-container'>
-             <h1 className='text-8xl w-full mb-10'>{data?.NICKEL}</h1>
-                    <div className='flex gap-12 w-full mb-20 mt-20'>
-                          <div className="flex flex-col card w-72 h-48 ml-4 mb-6 bg-base-100 border-primary border-2 items-center justify-center">
-                                <div className='flex flex-col'>
-                                                    <h1 className='text-xl'>28</h1>
-                                                    <p className='text-primary text-8xl font-bold'>Ni</p>
-                                            </div>
+  return (
+    <section className='max-container md:padding-container'>
+      <h1 className='text-4xl md:text-6xl lg:text-8xl w-full mb-10 text-center'>{data?.NICKEL}</h1>
+      
+      <div className='flex flex-col lg:flex-row gap-12 mb-20 mt-20 items-center'>
+        <div className="flex flex-col card w-72 h-48 mb-6 bg-base-100 border-primary border-2 items-center justify-center">
+          <div className='flex flex-col items-center'>
+            <h1 className='text-xl'>28</h1>
+            <p className='text-primary text-6xl font-bold'>Ni</p>
+          </div>
+          <div className="flex w-full card-actions justify-end pr-4">
+            <p className="text-xl">58.59</p>
+          </div>
+        </div>
 
-                                            <div className="flex w-full card-actions mr-8 justify-end">
-                                                    <p className="text-xl ">58.59</p>
-                                                </div>
-                                     </div>
+        <div className='flex flex-2 mt-10 lg:mt-0'>
+          <p className='px-3 lg:px-10 font-normal text-lg text-center lg:text-left'>{data?.NICKEL_DESC}</p>
+        </div>
+      </div>
 
-                            <div className='flex-2 flex mt-10'>
-                                    <p className='px-10 font-normal text-lg'>{data?.NICKEL_DESC}</p>
-                            </div>
-                    </div>
-                    
-                    <div className='flex flex-col rounded border-solid border-2 border-base-200 max-container padding-container py-4 mb-4'>
-                            <div className='flex mt-6'>
-                                    <h1 className='text-xl md:text-4xl mr-auto ml-auto font-semibold text-gray-400 text-center lg:mb-8 mb-4 tracking-wider'>{data?.OUR_PRODUCTS}</h1>
-                            </div>
-                            
-                            <div className='flex flex-col gap-10 mt-6 pb-3'>
-                                    {data?.NICKEL_PRODUCTS.map((product,index) => (
-                                            <div key={product.symbol} className={`flex gap-7 items-center justify-center ${index !== data?.NICKEL_PRODUCTS.length - 1 ? 'border-b-2 border-b-gray-300 pb-8' : ''}`} >
-                                                    <div  className='flex card w-48 h-20 bg-base-100 border-primary border-2 justify-center items-center'  >
-                                                            <div className=''>
-                                                                    <h2 className='text-neutral font-light text-lg'>{product.symbol}</h2>
-                                                            </div>
-                                                    </div>
+      <div className='flex flex-col rounded border-solid border-2 border-base-200 py-4 mb-4'>
+        <div className='flex justify-center'>
+          <h1 className='text-xl md:text-4xl font-semibold text-gray-400 text-center mb-4 lg:mb-8 tracking-wider'>{data?.OUR_PRODUCTS}</h1>
+        </div>
 
-                                                    <div className='flex flex-1 flex-col items-start justify-center'>
-                                                         <h1 className='text-xl font-semibold'>{product.name}</h1>
-                                                         <h4>CAS: {product.cas}</h4>
-                                                    </div>
+        <div className='flex flex-col gap-10 mt-6 pb-3'>
+          {data?.NICKEL_PRODUCTS.map((product, index) => (
+            <div key={product.symbol} className={`flex flex-col md:flex-row gap-7 items-center justify-center ${index !== data?.NICKEL_PRODUCTS.length - 1 ? 'border-b-2 border-b-gray-300 pb-8' : ''}`}>
+              <div className='flex card w-48 h-20 bg-base-100 border-primary border-2 justify-center items-center'>
+                <div className='text-center'>
+                  <h2 className='text-neutral font-light text-lg'>{product.symbol}</h2>
+                </div>
+              </div>
 
-                                                    <div className='flex flex-1 flex-col'>
-                                                            <h1 className='text-xl font-semibold mb-2'>{data?.USAGE}</h1>
-                                                            <div className='flex flex-col'>
-                                                                    <ol className='list-disc ml-7'>
-                                                                            {product.use.map((use,index) => (
-                                                                                    <li key={index} className='text-base '>{use}</li>
-                                                                            ))}
-                                                                    </ol>
-                                                            </div>
-                                                    </div>
-                                            </div>
-                                    ))}
-                            </div>
-                    </div>
+              <div className='flex flex-1 flex-col text-center md:text-left'>
+                <h1 className='text-xl font-semibold'>{product.name}</h1>
+                <h4>CAS: {product.cas}</h4>
+              </div>
+
+              <div className='flex flex-1 flex-col mr-auto ml-5 md:ml-0'>
+                <h1 className='text-xl font-semibold mb-2'>{data?.USAGE}</h1>
+                <div className='flex flex-col'>
+                  <ol className='list-disc ml-7'>
+                    {product.use.map((use, index) => (
+                      <li key={index} className='text-base'>{use}</li>
+                    ))}
+                  </ol>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
-)
+  )
 }
 
-export default NickelInfo
+export default NickelInfo;
--- components/OtherProductInfo.tsx
+++ components/OtherProductInfo.tsx
@@ -1,44 +1,44 @@
-"use client";
+'use client';
 import { useLocale } from '../lang/LocaleContext';
-import React from 'react'
+import React from 'react';
 
 const OtherProductInfo = () => {
- const { data } = useLocale() || {};
+  const { data } = useLocale() || {};
   return (
-    <section className='max-container'>
-    <h1 className='text-8xl w-full mb-20'>{data?.OUR_OTHER_PRODUCTS}</h1>
-           
-           <div className='flex flex-col rounded border-solid border-2 border-base-200 max-container padding-container py-4 mb-4'>
-                   
-                   <div className='flex flex-col gap-10 mt- pb-3'>
-                           {data?.OTHER_PRODUCTS.map((product,index) => (
-                                   <div key={product.symbol} className={`flex gap-7 items-center justify-center ${index !== data?.OTHER_PRODUCTS.length - 1 ? 'border-b-2 border-b-gray-300 pb-8' : ''}`} >
-                                           <div  className='flex card w-48 h-20 bg-base-100 border-primary border-2 justify-center items-center'  >
-                                                   <div className=''>
-                                                           <h2 className='text-neutral font-light text-lg'>{product.symbol}</h2>
-                                                   </div>
-                                           </div>
+    <section className='max-container md:padding-container'>
+      <h1 className='text-4xl md:text-6xl lg:text-8xl w-full mb-10 text-center'>{data?.OUR_OTHER_PRODUCTS}</h1>
+      
+      <div className='flex flex-col rounded border-solid border-2 border-base-200 py-4 mb-4'>
+        <div className='flex flex-col gap-10 mt-6 pb-3'>
+          {data?.OTHER_PRODUCTS.map((product, index) => (
+            <div key={product.symbol} className={`flex flex-col md:flex-row gap-7 items-center justify-center ${index !== data?.OTHER_PRODUCTS.length - 1 ? 'border-b-2 border-b-gray-300 pb-8' : ''}`}>
+              <div className='flex card w-48 h-20 bg-base-100 border-primary border-2 justify-center items-center'>
+                <div className='text-center'>
+                  <h2 className='text-neutral font-light text-lg'>{product.symbol}</h2>
+                </div>
+              </div>
 
-                                           <div className='flex flex-1 flex-col items-start justify-center'>
-                                                <h1 className='text-xl font-semibold'>{product.name}</h1>
-                                                <h4>CAS: {product.cas}</h4>
-                                           </div>
+              <div className='flex flex-1 flex-col text-center md:text-left'>
+                <h1 className='text-xl font-semibold'>{product.name}</h1>
+                <h4>CAS: {product.cas}</h4>
+              </div>
 
-                                           <div className='flex flex-1 flex-col'>
-                                                   <h1 className='text-xl font-semibold mb-2'>{data?.USAGE}</h1>
-                                                   <div className='flex flex-col'>
-                                                           <ol className='list-disc ml-7'>
-                                                                   {product.use.map((use,index) => (
-                                                                           <li key={index} className='text-base '>{use}</li>
-                                                                   ))}
-                                                           </ol>
-                                                   </div>
-                                           </div>
-                                   </div>
-                           ))}
-                   </div>
-           </div>
-</section>
-)
+              <div className='flex flex-1 flex-col mr-auto ml-5 md:ml-0'>
+                <h1 className='text-xl font-semibold mb-2'>{data?.USAGE}</h1>
+                <div className='flex flex-col'>
+                  <ol className='list-disc ml-7'>
+                    {product.use.map((use, index) => (
+                      <li key={index} className='text-base'>{use}</li>
+                    ))}
+                  </ol>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
-export default OtherProductInfo
+
+export default OtherProductInfo;
